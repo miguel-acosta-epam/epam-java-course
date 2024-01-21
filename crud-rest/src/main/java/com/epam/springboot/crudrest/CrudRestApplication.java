a77a92583e7c2ba57cb2704a7d84d7664b72a15d
Miguel Acosta <miguel_acosta@epam.com>
2024-01-20 22:34:57 -0500

    feat(crud-rest): CRUD rest initial config from spring.io

---
+++ crud-rest/src/main/java/com/epam/springboot/crudrest/CrudRestApplication.java
@@ -0,0 +1,13 @@
+package com.epam.springboot.crudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRestApplication.class, args);
+	}
+
+}
