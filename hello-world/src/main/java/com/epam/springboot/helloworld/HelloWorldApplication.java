ae62bbee051ac560d8033cd8e25748c9b6bb60ca
Miguel Acosta <miguel_acosta@epam.com>
2024-01-20 22:21:24 -0500

    feat: Hello world CLR imported from Springboot.io

---
+++ hello-world/src/main/java/com/epam/springboot/helloworld/HelloWorldApplication.java
@@ -0,0 +1,23 @@
+package com.epam.springboot.helloworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner helloWorld() throws Exception {
+        return args -> {
+            System.out.println("Hello world");
+        };
+    }
+
+
+}
