bdd81248662a66cac9ef1dc853093187c02761b4
Miguel Acosta <miguel_acosta@epam.com>
2024-01-23 17:24:25 -0500

    feat(security): SecurityConfig working

--- crud-rest/src/main/java/com/epam/springboot/crudrest/config/SecurityConfig.java
+++ crud-rest/src/main/java/com/epam/springboot/crudrest/config/SecurityConfig.java
@@ -1,11 +1,19 @@
 package com.epam.springboot.crudrest.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
@@ -21,9 +29,18 @@ public class SecurityConfig {
                     .permitAll()
                     .anyRequest()
                     .authenticated())
-        .logout(l -> l.logoutUrl("/").permitAll())
+        .logout(LogoutConfigurer::permitAll)
         .oauth2Login(Customizer.withDefaults())
         .formLogin(Customizer.withDefaults())
         .build();
+  }
+
+  @Autowired
+  public void ConfigureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.inMemoryAuthentication()
+        .passwordEncoder(NoOpPasswordEncoder.getInstance())
+        .withUser("user")
+        .password("password")
+        .roles("USER");
   }
 }
